f3b81dccf88dc4356cc3b3a792730251140f6d62
acengberak <adit.goyo@gmail.com>
2020-01-16 16:03:16 +0700

    login dan logout auth dan mailtrap verify

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -30,18 +30,39 @@
                 <a class="nav-link" href="#">Testimonial</a>
             </li>
         </ul>
-       
+
+        
         <!-- Mobile button -->
+        @guest
         <form class="form-inline d-sm-block d-md-none">
-            <button class="btn btn-login my-2 my-sm-0">
+            <button class="btn btn-login my-2 my-sm-0" type="button" onclick="event.preventDefault();location.href='{{url('login')}}';">
                 Masuk
             </button>
         </form>
         <!-- Desktop Button -->
         <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-            <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
+            <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button" onclick="event.preventDefault();location.href='{{url('login')}}';">
                 Masuk
             </button>
         </form>
+        @endguest
+
+
+        @auth
+        <!-- Mobile button -->
+        <form class="form-inline d-sm-block d-md-none" action="{{ url('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-login my-2 my-sm-0" type="button" type="submit">
+                Logout
+            </button>
+        </form>
+        <!-- Desktop Button -->
+        <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
+                Logout
+            </button>
+        </form>
+        @endauth
     </div>
 </nav>
