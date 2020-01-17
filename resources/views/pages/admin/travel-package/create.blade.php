9df2fe9683475afb6aa039207cda883a94ada0b3
acengberak <adit.goyo@gmail.com>
2020-01-17 17:27:01 +0700

    create blade fix form-group

--- resources/views/pages/admin/travel-package/create.blade.php
+++ resources/views/pages/admin/travel-package/create.blade.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Tambah Travel</h1>
+        <h1 class="h3 mb-0 text-gray-800">Tambah Travel Package</h1>
     </div>
     <div class="row">
         <div class="card-body">
@@ -29,51 +29,69 @@
                 <div class="card shadow">
                     <div class="card-body">
                         <form action="{{ route('travel-package.store') }}" method="post">
-                            @csrf
+                         @csrf
+                         {{-- TITTLE --}}
                             <div class="form-group">
                               <label for="title">Title</label>
-                              <input type="text" class="form-control" name="title" placeholder="Title" value="{{ old('title') }}">
+                              <input type="text" name="title" id="title" class="form-control" placeholder="Isi Title" aria-describedby="helpId" value="{{ old('title') }}">
+                              <small id="helpId" class="text-muted">Title</small>
                             </div>
-                            <div class="form-group">
-                              <label for="location">Location</label>
-                              <input type="text" class="form-control" name="location" placeholder="Location" value="{{ old('location') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="about">About</label>
-                              <textarea name="about" rows="10" class="d-block w-100 form-control">{{ old('about') }}</textarea>
-                            </div>
-                            <div class="form-group">
-                              <label for="featured_event">Featured Event</label>
-                              <input type="text" class="form-control" name="featured_event" placeholder="Featured Event" value="{{ old('featured_event') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="language">Language</label>
-                              <input type="text" class="form-control" name="language" placeholder="Language" value="{{ old('language') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="foods">Foods</label>
-                              <input type="text" class="form-control" name="foods" placeholder="Foods" value="{{ old('foods') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="departure_date">Departure Date</label>
-                              <input type="date" class="form-control" name="departure_date" placeholder="Departure Date" value="{{ old('departure_date') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="duration">Duration</label>
-                              <input type="text" class="form-control" name="duration" placeholder="Duration" value="{{ old('duration') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="type">Type</label>
-                              <input type="text" class="form-control" name="type" placeholder="Type" value="{{ old('type') }}">
-                            </div>
-                            <div class="form-group">
-                              <label for="price">Price</label>
-                              <input type="text" class="form-control" name="price" placeholder="Price" value="{{ old('price') }}">
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">
-                              Save
-                            </button>
-                          </form>
+                         {{-- LOCATION --}}
+                         <div class="form-group">
+                            <label for="location">Location</label>
+                            <input type="text" name="location" id="location" class="form-control" placeholder="Isi Lokasi" aria-describedby="helpId" value="{{ old('location') }}">
+                            <small id="helpId" class="text-muted">Lokasi</small>
+                          </div>
+                          {{-- About --}}
+                          <div class="form-group">
+                            <label for="about">About</label>
+                            <textarea name="about" id="about" class="form-control" placeholder="Isi About" aria-describedby="helpId" value="{{ old('about') }}" rows="10"></textarea>
+                            <small id="helpId" class="text-muted">About</small>
+                          </div>
+                          {{-- FEATURED EVENT --}}
+                          <div class="form-group">
+                            <label for="featured_event">Featured Event</label>
+                            <input type="text" name="featured_event" id="featured_event" class="form-control" placeholder="Featured Event" aria-describedby="helpId" value="{{ old('featured_event') }}">
+                            <small id="helpId" class="text-muted">Featured Event</small>
+                          </div>
+                          {{-- Language --}}
+                          <div class="form-group">
+                            <label for="language">Language</label>
+                            <input type="text" name="language" id="language" class="form-control" placeholder="Isi Language" aria-describedby="helpId" value="{{ old('language') }}">
+                            <small id="helpId" class="text-muted">Language</small>
+                          </div>
+                          {{-- FOODS --}}
+                          <div class="form-group">
+                            <label for="foods">Foods</label>
+                            <input type="text" name="foods" id="foods" class="form-control" placeholder="Isi Foods" aria-describedby="helpId" value="{{ old('foods') }}">
+                            <small id="helpId" class="text-muted">Foods</small>
+                          </div>
+                          {{-- DEPARTURE DATE --}}
+                          <div class="form-group">
+                            <label for="departure_date">Departure Date</label>
+                            <input type="date" name="departure_date" id="departure_date" class="form-control" placeholder="Isi Departure Date" aria-describedby="helpId" value="{{ old('departure_date') }}">
+                            <small id="helpId" class="text-muted">Departure Date</small>
+                          </div>
+                          {{-- DURATION --}}
+                          <div class="form-group">
+                            <label for="duration">Duration</label>
+                            <input type="text" name="duration" id="duration" class="form-control" placeholder="Isi Duration" aria-describedby="helpId" value="{{ old('duration') }}">
+                            <small id="helpId" class="text-muted">Duration</small>
+                          </div>
+                          {{-- TYPE --}}
+                          <div class="form-group">
+                            <label for="type">Type</label>
+                            <input type="text" name="type" id="type" class="form-control" placeholder="Isi Type" aria-describedby="helpId" value="{{ old('type') }}">
+                            <small id="helpId" class="text-muted">Type</small>
+                          </div>
+                          {{-- PRICE --}}
+                          <div class="form-group">
+                            <label for="price">Price</label>
+                            <input type="text" name="price" id="price" class="form-control" placeholder="Isi Price" aria-describedby="helpId" value="{{ old('price') }}">
+                            <small id="helpId" class="text-muted">Price</small>
+                          </div>
+                          <button type="submit" class="btn btn-primary btn-block">Simpan</button>
+                        </form>
                     </div>
                 </div>
             </div>
