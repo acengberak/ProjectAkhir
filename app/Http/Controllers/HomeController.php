c1d47b67044b6af4ff894c293eedf89f84c7fe5a
acengberak <adit.goyo@gmail.com>
2020-01-22 14:06:56 +0700

    frontend images -> database untuk detail travel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Admin\TravelPackageRequest;
 use Illuminate\Http\Request;
+use App\TravelPackage;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        $items = TravelPackage::with(['galleries'])->get();
+        return view('pages.home', [
+            'items' => $items
+        ]);
     }
 }
